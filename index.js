42f18e13393806106cb2b4dc0891aaea22d3cb89
patclittle <plittle@Patricks-MacBook-Pro-7.local>
2016-11-30 18:26:54 -0800

    filled in the template a bit

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.get('/', function(request, response) {
   response.render('index');
 });
 
+app.get('/resume', function(request, response) {
+  response.render('resume/resume');
+});
+
+app.get('/grades', function(request, response) {
+  response.render('grades/grades');
+});
+
+app.get('/contact', function(request, response) {
+  response.render('contact/contact');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
